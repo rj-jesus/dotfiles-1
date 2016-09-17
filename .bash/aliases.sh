ec9c0e28ca3b7668c11202279cfefddfa0ca5893
Pedro Martins <pbmartins@ua.pt>
2016-09-17 23:16:05 +0100

    Added new file organization system

---
+++ .bash/aliases.sh
@@ -0,0 +1,39 @@
+# Aliases
+alias finder="open -a Finder"
+alias cask="brew cask"
+alias lla='ls -la'
+alias mkdir='mkdir -pv'
+alias psp='ps aux | grep'
+alias gcca='gcc -ansi -Wall'
+alias reload='source ~/.bashrc' 
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Shortcuts
+alias ua="cd ~/Documents/UA"
+alias proj="cd ~/Documents/Projects"
+alias iia="cd ~/Documents/UA/Ano_3/IIA"
+alias so="cd ~/Documents/UA/Ano_3/SO"
+
+# Easier navigation
+alias 1.="cd .."
+alias 2.="cd ../.."
+alias 3.="cd ../../.."
+alias 4.="cd ../../../.."
+
+# Get rid of those pesky .DS_Store files recursively
+alias dsclean='find . -type f -name .DS_Store -delete'
+
+# Flush your dns cache
+alias flush='dscacheutil -flushcache'
+
+# Show/hide hidden files
+alias showhidden="defaults write com.apple.finder AppleShowAllFiles TRUE"
+alias hidehidden="defaults write com.apple.finder AppleShowAllFiles FALSE"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload="exec $SHELL -l"
---
+++ .bash/prompt.sh
@@ -0,0 +1,53 @@
+# BASH PROMPT
+
+_prompt() {
+    readonly OS_SYMBOL=''
+    readonly GIT_BRANCH_SYMBOL='⎇ '
+    readonly GIT_BRANCH_NORMAL_SYMBOL='✓'
+    readonly GIT_BRANCH_CHANGED_SYMBOL='+'
+    readonly GIT_STASH_SYMBOL="@"
+    readonly GIT_PUSH_SYMBOL='↑'
+    readonly GIT_PULL_SYMBOL='↓'
+
+    _git_branch_info() {
+        local branch="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
+        [ -n "$branch" ] || return
+
+        local marks=" $GIT_BRANCH_NORMAL_SYMBOL"
+
+        # Check if branch is modified.
+        [ -n "$(git status --porcelain)" ] && marks=" $GIT_BRANCH_CHANGED_SYMBOL"
+
+        # Compute how many commits local branch is ahead/behind of remote branch.
+        local stat="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
+        local aheadN="$(echo $stat | grep -o 'ahead \d\+' | grep -o '\d\+')"
+        local behindN="$(echo $stat | grep -o 'behind \d\+' | grep -o '\d\+')"
+        [ -n "$aheadN" ] && marks+=" $GIT_PUSH_SYMBOL$aheadN"
+        [ -n "$behindN" ] && marks+=" $GIT_PULL_SYMBOL$behindN"
+
+        # Print the git branch segment without a trailing newline
+        printf " $GIT_BRANCH_SYMBOL $branch$marks "
+    }
+
+    ps1() {
+        if [ $? -eq 0 ]; then
+            local BG_EXIT="$GREEN_BACKGROUND"
+        else
+            local BG_EXIT="$RED_BACKGROUND"
+        fi
+
+        PS1="\[$BLUE\]\u@\h \[$NORMAL\]" # User@host
+        PS1+="\[$WHITE\]\[$LIGHT_GREY_BACKGROUND\] \w \[$NORMAL\]" # Directory
+
+        if [[ -z $branch ]]; then
+            PS1+="\[$GOLD\]\[$GREY_BACKGROUND\]\[$BOLD\]$(_git_branch_info)\[$NORMAL\]" # Git Branch Info.
+        fi
+
+        PS1+="\[$BG_EXIT\] $OS_SYMBOL \[$NORMAL\] "
+    }
+
+    PROMPT_COMMAND=ps1
+}
+
+_prompt
+unset _prompt
